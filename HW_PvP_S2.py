081138fc814dfa66d002cf0ee126ead2621424b0
Vladimir <dv.quiz@yandex.ru>
2023-09-24 23:19:43 +0300

    HW_PvP_S2_task2

--- HW_PvP_S2.py
+++ HW_PvP_S2.py
@@ -1,20 +1,39 @@
-# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
-# Функцию hex используйте для проверки своего результата.
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
 
+fraction1 = input('Введите первую дробь через /: ')
+fraction2 = input('Введите вторую дробь через /: ')
 
-# Проверка
-# num = int(input())
-# hex_num = hex(num)
-# print(hex_num)
+numerator_1_fraction1 = int(fraction1.split('/')[0])
+denominator_1_fraction1 = int(fraction1.split('/')[1])
+numerator_2_fraction2 = int(fraction2.split('/')[0])
+denominator_2_fraction2 = int(fraction2.split('/')[1])
 
-num = int(input("Введите десятичное число: "))
-number_system = "0123456789ABCDEF"
-hexadecimal_system = ""
+# Сложение дробей
 
-while num > 0:
-    remainder = num % 16
-    hexadecimal_digit = number_system[remainder]
-    hexadecimal_system = hexadecimal_digit + hexadecimal_system
-    num //= 16
+if denominator_1_fraction1 != denominator_2_fraction2:
+    common_numerator = (numerator_1_fraction1 * denominator_2_fraction2) + (numerator_2_fraction2 * denominator_1_fraction1)
+    common_denominator = denominator_1_fraction1 * denominator_2_fraction2
+else:
+    common_numerator = numerator_1_fraction1 + numerator_2_fraction2
+    common_denominator = denominator_1_fraction1
+    print(f'{common_numerator}/{common_denominator}')
 
-print("Шестнадцатеричное число:", hexadecimal_system)
+# Произведение дробей
+product_of_numerators = numerator_1_fraction1 * numerator_2_fraction2
+product_of_divisors = denominator_1_fraction1 * denominator_2_fraction2
+
+# Сокращение
+
+for i in range(2, common_numerator + 1):
+    while common_numerator % i == 0 and common_denominator % i ==0:
+        common_numerator //= i
+        common_denominator //= i
+
+for i in range(2, product_of_numerators + 1):
+    while product_of_numerators % i == 0 and product_of_divisors % i ==0:
+        product_of_numerators //= i
+        product_of_divisors //= i
+
+print(f'Сложение дробей {fraction1} и {fraction2} = {common_numerator}/{common_denominator}')
+print(f'Произведение дробей {fraction1} и {fraction2} = {product_of_numerators}/{product_of_divisors}')
